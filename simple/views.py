0f89bf1f5fcf5597aceac41b10eea15c700bada3
Benson Mburu <nguruben@gmail.com>
2015-10-08 19:34:03 +0300

    fetching resources done

--- simple/views.py
+++ simple/views.py
@@ -30,3 +30,49 @@ elif result.user:
         response.writ(u'<h2>Your id is:{0}</h2>'.format(result.user.id))
         response.write(u'<h2>YOur Email is:{0}</h2>'.format(reult.user.email))
         #Next i access protected resources
+        if result.user.credentials:
+            #Get the five most recent facebook statuses
+        if result.provider.name =='fb':
+            response.write('You are logged in with facebook. <br>')
+            url='https://graph.facebook.com/{0}?fields=feed.limit(5)'
+            url=url.format(result.user.id)
+            #access protected resources
+            access_response=result.provider.access(url)
+            #Ill parse the list on a JSON
+            if access_response.status ==200;
+            #Actual response parsing
+            staus ==access_response.data.get('feed').get('data')
+            error=access_response.data.get('error')
+            if error:
+                response.write(u'error{0}'.format(error))
+            elif statuses:
+                response.write('Your five most recent tweets')
+                for messages in statuses
+                text=message.get('message')
+                date=message.get('created_time')
+                response.write(u'<h3>{0}</h3>.format(text))
+                response.write(u'posted on:{0}.format(date))
+            else
+                response.write('ERROR')
+                response.write(u'Status:{0}.format(response.status))
+        if result.provider.name=='tw':
+                 response.write('you are logged on with twitter')
+            #fetch five most recent tweets
+                  url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+                access_response =result.provider.access(url,{'count':5})
+                if access_response.status==200;
+                   if type(access_response.data) is list:
+                      response.write("5 tweets")
+                    for tweet in access_response.data
+                    text=tweet.get('text')
+                    date=tweet.get('created_at')
+                    response.write(u'<h3>{0}</h3>'.format(text))
+                    response.write(u'Tweeted on"{0}.format(date))
+                elif response.data.get('errors'):
+                     response.write('Error Twitter:{0}'.\
+                                   format(response.data.get('errors')))
+                else:
+                    response.write('Error Twitter B')
+                    response.write(u'status:{0}.format(response.status))
+                                   
+                return response
